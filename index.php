4f6fdca9534bb8201e9a9be9d3db8c5c6b17dfcd
Mohd Iqbal Mohsin <mohdiqbal.binmohsin@gmail.com>
2025-10-15 17:31:14 +0800

    Change the Web Page Title
    Done by IQBAL - 15102025 - 1730

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Security Check Tool</title>
+<title>IQBAL Security Check Tool</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Azure WebApp Security Checker</h1>
+<h1>IQBAL WebApp Security Checker</h1>
 <form action="scan.php" method="get">
   <label>Target URL or IP:</label><br>
   <input type="text" name="target" placeholder="https://example.com or 8.8.8.8" required>
