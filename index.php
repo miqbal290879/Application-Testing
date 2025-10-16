d0ae7bcf3b99c4b08e91f752a0cc4884dee37848
Mohd Iqbal Mohsin <mohdiqbal.binmohsin@gmail.com>
2025-10-16 10:31:12 +0800

    Update to IQBAL MOHSIN

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>IQBAL Security Check Tool</title>
+<title>IQBAL MOHSIN Security Check Tool</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>IQBALTESTING WebApp Security Checker</h1>
+<h1>IQBAL MOHSIN WebApp Security Checker</h1>
 <form action="scan.php" method="get">
   <label>Target URL or IP:</label><br>
   <input type="text" name="target" placeholder="https://example.com or 8.8.8.8" required>
